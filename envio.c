ad62058c85fe8b3ae6389964937fe13dee518c30
Laura Lopes <lauraslopes94@gmail.com>
2022-04-20 17:18:56 -0300

    Verifica peso da carga e ordem de itens. Erro no free.

--- envio.c
+++ envio.c
@@ -15,26 +15,82 @@ void copia(t_parciais * parciais, int qtdItens) {
 	}
 }
 
-void resolucao(int qtdItens, double capacidade, double * pesos, int qtdPares, t_par_ordenado * pares, t_parciais * parciais, int itemIndice, int l, double custo) {
+int verificaPeso(double * pesos, t_parciais * parciais, double capacidade, int viagem, int l) {
+    int soma = 0;
+    int i;
+    for (i = 0; i < l; i++) {
+        if (parciais[i].viagem == viagem){
+            soma += pesos[parciais[i].indice - 1];
+        }
+    }
 
-    if (itemIndice > qtdItens) {
+    printf("Soma do peso da viagem %d com o item %d: %d / %lf.\n", viagem, i, soma, capacidade);
+    if (soma > capacidade)
+        return 0;
+
+    return 1;
+}
+
+int verificaOrdem(int qtdPares, t_par_ordenado * pares, int l, t_parciais * parciais, int itemIndice, int viagem) {
+
+    for (int i = 0; i < qtdPares; i++) {
+        if (pares[i].depois == itemIndice) {
+            for (int j = 0; j < l; j++) {
+                if ((parciais[j].indice == pares[i].antes) && (parciais[j].viagem >= viagem)){
+                    printf("Item %d não pode ir antes de %d\n", itemIndice, parciais[j].indice);
+                    return 0;
+                }
+            }
+        }
+    }
+    return 1;
+}
+
+int verificaCiclos(int qtdPares, t_par_ordenado * pares) {
+
+    for (int i = 0; i < qtdPares; i++) {
+        for (int j = i+1; j < qtdPares; j++) {
+            if ((pares[i].antes == pares[j].depois) && (pares[i].depois == pares[j].antes)){
+                return 0;
+            }
+        }
+    }
+
+    return 1;
+}
+
+void resolucao(int qtdItens, double capacidade, double * pesos, int qtdPares, t_par_ordenado * pares, t_parciais * parciais, int l, int * ordemItens, int itemIndice, double custo) {
+
+    if (l > qtdItens) {
         if (custo < opt) {
             opt = custo;
             copia(parciais, qtdItens);
         }
-
         return;
     }
 
-    parciais[l].indice = itemIndice;
+    parciais[l].indice = ordemItens[itemIndice];
     double resultado;
     for (int i = 1; i <= qtdItens; i++) {
         parciais[l].viagem = i;
-        resultado = parcial(qtdItens, capacidade, pesos, qtdPares, pares, l+1, parciais);
-        if ((resultado > 0) && (resultado < opt)) {
-            resolucao(qtdItens, capacidade, pesos, qtdPares, pares, parciais, itemIndice+1, l+1, resultado);
+
+        if (verificaPeso(pesos, parciais, capacidade, i, l+1) && verificaOrdem(qtdPares, pares, l+1, parciais, ordemItens[itemIndice], i)){
+            resultado = parcial(qtdItens, capacidade, pesos, qtdPares, pares, l+1, parciais);
+            if ((resultado > 0) && (resultado < opt)) {
+                resolucao(qtdItens, capacidade, pesos, qtdPares, pares, parciais, l+1, ordemItens, itemIndice+1, resultado);
+            }
         }
     }
+}
+
+int buscaItem(int * ordemItens, int qtdItens, int k) {
+    
+    for (int j = 0; j < qtdItens; j++){
+        if (ordemItens[j] == k) {
+            return 1;
+        }
+    }
+    return 0;
 }
 
 void imprime(int qtdItens) {
@@ -69,18 +125,39 @@ int main() {
         scanf("%d", &pares[i].depois);
     }
 
-    opt = qtdItens;
+    int * ordemItens = (int *) calloc(qtdItens, sizeof(int));
+    int i;
+    for (i = 0; i < qtdPares; i++) {
+        ordemItens[i] = pares[i].antes;
+    }
 
-    t_parciais * parciais = (t_parciais *) calloc(qtdItens, sizeof(t_parciais));
-    resposta = (t_parciais *) calloc(qtdItens, sizeof(t_parciais));
-    int l = 0;
-    resolucao(qtdItens, capacidade, pesos, qtdPares, pares, parciais, 1, l, qtdItens);
-    imprime(qtdItens);
+    for (int k = 1 ; k <= qtdItens; k++) {
+        if (!buscaItem(ordemItens, qtdItens, k)) {
+            ordemItens[i] = k;
+            i++;
+        }
+    }
+
+    if(verificaCiclos(qtdPares, pares)){
+
+        opt = qtdItens;
+
+        t_parciais * parciais = (t_parciais *) calloc(qtdItens, sizeof(t_parciais));
+        resposta = (t_parciais *) calloc(qtdItens, sizeof(t_parciais));
+        int l = 0;
+        resolucao(qtdItens, capacidade, pesos, qtdPares, pares, parciais, l, ordemItens, 0, opt);
+        imprime(qtdItens);
+
+        free(parciais);
+        free(resposta);
+    }
+    else {
+        printf("Tem ciclo.\n");
+    }
 
     free(pesos);
     free(pares);
-    free(parciais);
-    free(resposta);
+    free(ordemItens);
 
     return 0;
 }
